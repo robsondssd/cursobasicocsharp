11fd2bca69ecdc883b19f88a874a9da58879f992
robsonsantos <robsonsantos@solucaosistemas.net>
2016-05-12 16:24:36 -0300

    Inicial

---
+++ Aula01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("teste do meu primero programa em  c#");
+            Console.ReadLine();
+        }
+    }
+}
